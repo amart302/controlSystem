fccf81bee232af32ef9f75c6ed6604286f119a49
Мартазанов Ахмед А <MartazanovAA23@ithub.local>
2025-04-22 10:28:58 +0300

    commit 2

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,7 +1,16 @@
-import { User } from "../models/User.js";
+// import { User } from "../models/User.js";
+
+import { validationResult } from "express-validator";
 
 export async function registration(req, res) {
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ 
+                message: errors.array()[0].msg,
+            });
+        }
+
         const { username, role, password, } = req.body;
         console.log(req.body);
         
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { registration } from '../controllers/controllers.js';
 
 const router = new Router();
 
-router.post("/registration", registration);
+router.post("/registration", [
+    body("username")
+        .trim()
+        .notEmpty().withMessage("Имя пользователя не должно быть пустым"),
+    body("role")
+        .trim()
+        .notEmpty().withMessage("Роль не должна быть пустой"),
+    body("password")
+        .isLength({ min: 6 })
+        .trim()
+        .notEmpty()
+], registration);
 
 export { router };
